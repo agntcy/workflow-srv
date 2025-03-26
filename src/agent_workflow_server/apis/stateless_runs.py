d7cb276cba3f3b07d90d2da9c6c2107372edf368
ataldir <ataldir@cisco.com>
2025-03-26 11:26:42 +0100

    add endpoint code

--- src/agent_workflow_server/apis/stateless_runs.py
+++ src/agent_workflow_server/apis/stateless_runs.py
@@ -257,7 +257,8 @@ async def search_stateless_runs(
     run_search_request: RunSearchRequest = Body(None, description=""),
 ) -> List[RunStateless]:
     """Search for stateless run.  This endpoint also functions as the endpoint to list all stateless Runs."""
-    raise HTTPException(status_code=500, detail="Not implemented")
+    runs = Runs.search(run_search_request)
+    return runs
 
 
 @router.get(
