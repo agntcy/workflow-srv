6c815d2532fec6cda94f05ae756efecd32ad7a27
ataldir <ataldir@cisco.com>
2025-05-27 17:17:02 +0200

    chore: llamaindex unit tests
    Signed-off-by: ataldir <ataldir@cisco.com>

---
+++ tests/agents/jokeflow.py
@@ -0,0 +1,49 @@
+# Copyright AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+
+import asyncio
+
+from llama_index.core.workflow import (
+    Event,
+    StartEvent,
+    StopEvent,
+    Workflow,
+    step,
+)
+
+
+class JokeEvent(Event):
+    joke: str
+
+
+class JokeFlow(Workflow):
+    # llm = OpenAI()
+
+    @step
+    async def generate_joke(self, ev: StartEvent) -> JokeEvent:
+        topic = ev.topic
+
+        # prompt = f"Write your best joke about {topic}."
+        # response = await self.llm.acomplete(prompt)
+        response = "this is a joke about " + topic
+        asyncio.sleep(1)
+        return JokeEvent(joke=str(response))
+
+    @step
+    async def critique_joke(self, ev: JokeEvent) -> StopEvent:
+        joke = ev.joke
+
+        # prompt = f"Give a thorough analysis and critique of the following joke: {joke}"
+        # response = await self.llm.acomplete(prompt)
+        response = "this is a critique of the joke: " + joke
+        return StopEvent(result=str(response))
+
+
+async def main():
+    w = JokeFlow(timeout=60, verbose=False)
+    result = await w.run(topic="pirates")
+    print(str(result))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
